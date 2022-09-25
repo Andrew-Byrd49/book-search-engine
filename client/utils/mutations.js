7490860e250f35a067cfb5c750ccc855ba166d61
Andrew Byrd <andrew.byrds2@gmail.com>
2022-09-25 18:20:16 -0400

    added to front-end

--- client/utils/mutations.js
+++ client/utils/mutations.js
@@ -1,18 +1,34 @@
 import { gql } from '@apollo/client';
 
-export const GET_ME = gql`
-  {
-    me {
+export const login_user = gql`
+  mutation login($email: String!, $password: String!) {
+    login(email: $email, password: $password) {
+      token
+      user {
         _id
         username
-        email
-        bookCount
-        bookId
-        authors
-        description
-        image
-        link
-        title
+      }
     }
   }
 `;
+
+export const add_user = gql`
+  mutation addUser($username: String!, $email: String!, $password: String!) {
+    addUser(username: $username, email: $email, password: $password) {
+      token
+      user {
+        _id
+        username
+      }
+    }
+  }
+`;
+
+export const save_book = gql`
+  mutation saveBook($input: saveBookInput!) {
+    saveBook(input: $input) {
+    }
+  }
+`;
+
+export const remove_book = gql
